fb750cee2ed413cb2d1b2b3d21715c434bb82ee5
Nick-Menzhuber <nickmenzhuber@gmail.com>
2022-11-20 10:00:30 -0600

    initial commit, package dependecies deployed, env in place, basic file structure

---
+++ index.js
@@ -0,0 +1,7 @@
+//dotenv setup
+require('dotenv').config();
+
+//define module dependecies
+const cTable = require('console.table');
+const inquirer = require('inquirer');
+const mysql = require('mysql2')
