09801662e28fa4f11e72b9c8c6fe9cd39e796c20
Nick-Menzhuber <nickmenzhuber@gmail.com>
2022-11-21 12:03:32 -0600

    adds basic structure to schema

--- db/schema.sql
+++ db/schema.sql
@@ -1,6 +1,28 @@
+/*initialize database*/
 DROP DATABASE IF EXISTS employees_db;
 CREATE DATABASE employees_db;
 
 USE employees_db;
 
-CREATE TABLE
+/*create department table*/
+CREATE TABLE department (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(30) NOT NULL
+);
+
+/*create role table*/
+CREATE TABLE role (
+    id INT NOT NULL AUTO_INCREMENT,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL NOT NULL,
+    department_id INT NOT NULL,
+);
+
+/*create employee table*/
+CREATE TABLE employee (
+    id INT,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    role_id INT,
+    manager_id INT
+);
--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 //dotenv setup
 require('dotenv').config();
 
+//sequelize setup
+const sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+)
+
 //define module dependecies
 const cTable = require('console.table');
 const inquirer = require('inquirer');
-const mysql = require('mysql2')
+const mysql = require('mysql2');
+const { Sequelize } = require('sequelize');
