3fb63661e75479810e7d20da48ac21b0bee097ce
Nick-Menzhuber <nickmenzhuber@gmail.com>
2022-11-21 12:41:49 -0600

    adds seed data, fixes schema ids that were not working as expected

--- db/schema.sql
+++ db/schema.sql
@@ -1,28 +1,35 @@
-/*initialize database*/
+/*initializes and sets employees database*/
 DROP DATABASE IF EXISTS employees_db;
 CREATE DATABASE employees_db;
 
 USE employees_db;
-
-/*create department table*/
+/*creates department table*/
 CREATE TABLE department (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(30) NOT NULL
+  id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+  dept VARCHAR(30) NOT NULL
 );
 
-/*create role table*/
-CREATE TABLE role (
-    id INT NOT NULL AUTO_INCREMENT,
-    title VARCHAR(30) NOT NULL,
-    salary DECIMAL NOT NULL,
-    department_id INT NOT NULL,
+/*creates roles table*/
+CREATE TABLE roles ( 
+  id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+  title VARCHAR(30) NOT NULL,
+  salary DECIMAL NOT NULL,
+  department_id INT,
+  FOREIGN KEY (department_id)
+  REFERENCES department(id)
+  ON DELETE SET NULL
 );
-
-/*create employee table*/
+/*creates employee table*/
 CREATE TABLE employee (
-    id INT,
-    first_name VARCHAR(30),
-    last_name VARCHAR(30),
-    role_id INT,
-    manager_id INT
+  id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  role_id INT,
+  manager_id INT,
+  FOREIGN KEY (role_id)
+  REFERENCES role(id)
+  ON DELETE SET NULL,
+  FOREIGN KEY (manager_id)
+  REFERENCES employee(id)
+  ON DELETE SET NULL
 );
